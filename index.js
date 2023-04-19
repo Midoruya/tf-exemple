3f83fa93a0c9dcaac07bf3f6bc15cf3f3865a9ac
timofeychesnokv <wladimi482002@gmail.com>
2023-04-19 19:26:12 +0300

    fix

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import * as tfNode from '@tensorflow/tfjs-node'
 import * as tf from '@tensorflow/tfjs'
 import * as fs from 'fs'
 
@@ -8,36 +9,72 @@ const dogsFiles = fs.readdirSync(dogsDir);
 const cats = catsFiles.map((file) => {
 	const filePath = `${catsDir}/${file}`;
 	const buffer = fs.readFileSync(filePath);
-	const decodedImage = tf.node.decodeImage(buffer);
-	const resizedImage = tf.image.resizeBilinear(decodedImage, [IMAGE_WIDTH, IMAGE_HEIGHT]);
-	return resizedImage;
+	const decodedImage = tfNode.node.decodeImage(buffer);
+	return tf.image.resizeBilinear(decodedImage, [ 200, 200 ]);
 });
-
 const dogs = dogsFiles.map((file) => {
 	const filePath = `${dogsDir}/${file}`;
 	const buffer = fs.readFileSync(filePath);
-	const decodedImage = tf.node.decodeImage(buffer);
-	const resizedImage = tf.image.resizeBilinear(decodedImage, [IMAGE_WIDTH, IMAGE_HEIGHT]);
-	return resizedImage;
+	const decodedImage = tfNode.node.decodeImage(buffer);
+	return tf.image.resizeBilinear(decodedImage, [ 200, 200 ]);
 });
-
 const images = cats.concat(dogs);
 const labels = tf.tensor2d(
-	Array.from({ length: cats.length }).fill([1, 0]).concat(Array.from({ length: dogs.length }).fill([0, 1]))
+	Array.from({ length: cats.length }).fill([1,0]).concat(Array.from({ length: dogs.length }).fill([0,1]))
 );
+console.log(labels.arraySync())
+const model = tf.sequential()
 
-const model = new tf.Sequential({layers: [
-		tf.layers.conv2d({filters: 16, kernelSize: [3,3], activation: 'relu', inputShape: [200,200,3]}),
-		tf.layers.maxPool2d({poolSize: [2,2]}),
-		tf.layers.conv2d({filters: 32, kernelSize: [3,3], activation: 'relu'}),
-		tf.layers.maxPool2d({poolSize: [3,3]}),
-		tf.layers.conv2d({filters: 64, kernelSize: [3,3], activation: 'relu'}),
-		tf.layers.maxPool2d({poolSize: [2,2]}),
-		tf.layers.dense({units: 512, activation: 'relu'}),
-		tf.layers.dense({units: 1, activation: 'sigmoid'}),
-	], name: 'animals'})
-model.compile({optimizer: new tf.train.adam, loss: tf.losses.sigmoidCrossEntropy, metrics: [ tf.metrics.binaryAccuracy ]})
-await model.fit(images, labels, {
-	epochs: 100,
-	shuffle: true
+model.add(tf.layers.conv2d({
+	inputShape: [200, 200, 3],
+	filters: 16,
+	kernelSize: 3,
+	activation: 'relu'
+}));
+model.add(tf.layers.maxPooling2d({
+	poolSize: 2,
+	strides: 2
+}));
+model.add(tf.layers.conv2d({
+	filters: 32,
+	kernelSize: 3,
+	activation: 'relu'
+}));
+model.add(tf.layers.maxPooling2d({
+	poolSize: 2,
+	strides: 2
+}));
+model.add(tf.layers.conv2d({
+	filters: 64,
+	kernelSize: 3,
+	activation: 'relu'
+}));
+model.add(tf.layers.maxPooling2d({
+	poolSize: 2,
+	strides: 2
+}));
+model.add(tf.layers.flatten());
+model.add(tf.layers.dense({
+	units: 512,
+	activation: 'relu'
+}));
+model.add(tf.layers.dense({
+	units: 2,
+	activation: 'sigmoid'
+}));
+
+model.compile({
+	optimizer: 'adam',
+	loss: tf.losses.sigmoidCrossEntropy,
+	metrics: ['accuracy']
+});
+await model.fit(tf.stack(images), labels, {
+	batchSize: 32,
+	epochs: 5,
+	validationData: [tf.stack(dogs),tf.tensor2d(Array.from({ length: dogs.length }).fill([0,1]))]
+	
 })
+console.log(await model.predict(tf.stack(dogs)).arraySync());
+//console.log(await model.predict(tf.stack(dogs)).data());
+//console.log(await model.predict(tf.stack(dogs)).data());
+console.log(await model.predict(tf.stack(cats)).arraySync());
